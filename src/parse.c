3973b10a6b97fccf11f25469b54f0bd3bd915560
Mark <alnyan@airmail.cc>
2020-07-02 21:09:09 +0300

    Add variable substitution

--- src/parse.c
+++ src/parse.c
@@ -1,20 +1,79 @@
 #include "parse.h"
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 #include <ctype.h>
+
+static ssize_t var_paste(char *dst, const char *name, size_t limit) {
+    size_t l;
+    if (!strcmp(name, "$")) {
+        size_t l = snprintf(dst, limit, "%d", getpid());
+        // Output was truncated
+        if (l > limit) {
+            l = limit;
+        }
+        return l;
+    }
+    const char *e = getenv(name);
+    if (!e) {
+        return 0;
+    }
+    l = strlen(e);
+    // Output was truncated
+    if (l > limit) {
+        l = limit;
+    }
+    strncpy(dst, e, limit);
+    return l;
+}
 
 static int cmd_unit_parse(const char *str, struct cmd_unit *unit) {
     // Parse until end of string or |
     size_t len;
     char *word = NULL;
+    char var[64];
+    size_t varlen;
     char c;
     unit->argc = 0;
 
     // TODO: escapes
-    // TODO: variable substitution
     while ((c = *str++) && c != '|') {
-        if (isspace(c)) {
+        if (c == '$') {
+            varlen = 0;
+            switch ((c = *str)) {
+            case '$':
+            case '?':
+                varlen = 1;
+                var[0] = c;
+                ++str;
+                break;
+            case '{':
+                ++str;
+                while ((c = *(str++)) && c != '}') {
+                    var[varlen++] = c;
+                }
+                break;
+            default:
+                while ((c = *str) && isalnum(c)) {
+                    var[varlen++] = c;
+                    ++str;
+                }
+                break;
+            }
+            var[varlen] = 0;
+
+            if (!word) {
+                word = malloc(256);
+                len = 0;
+            }
+
+            ssize_t res = var_paste(word + len, var, 255 - len);
+            if (res > 0) {
+                len += res;
+            }
+        } else if (isspace(c)) {
             if (word) {
                 word[len] = 0;
                 unit->args[unit->argc++] = word;
